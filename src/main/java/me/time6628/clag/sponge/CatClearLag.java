48180fb8a334259794eef789041ffeff846af028
Time6628 <pturchan@yahoo.com>
2017-02-18 00:47:02 -0500

    version 0.7.1

--- src/main/java/me/time6628/clag/sponge/CatClearLag.java
+++ src/main/java/me/time6628/clag/sponge/CatClearLag.java
@@ -52,7 +52,7 @@ import java.util.stream.Collectors;
 /**
  * Created by TimeTheCat on 7/2/2016.
  */
-@Plugin(name = "CatClearLag", id = "catclearlag", version = "0.7", description = "DIE LAG, DIE!")
+@Plugin(name = "CatClearLag", id = "catclearlag", version = "0.7.1", description = "DIE LAG, DIE!")
 public class CatClearLag {
 
     public static CatClearLag instance;
