d8f3e7d8067beb3bacd8d9cec31d759296cbb525
Time6628 <pturchan@yahoo.com>
2017-06-26 11:30:58 -0400

    fix laggychunks going to wrong coordinates.

--- src/main/java/me/time6628/clag/sponge/commands/LaggyChunksCommand.java
+++ src/main/java/me/time6628/clag/sponge/commands/LaggyChunksCommand.java
@@ -45,7 +45,8 @@ public class LaggyChunksCommand implements CommandExecutor {
             Player player = (Player) commandSource;
             Location<World> a = new Location<>(chunk.getWorld(), chunk.getPosition());
             Location<World> b = new Location<>(a.getExtent(), a.getX() * 16, a.getExtent().getBlockMax().getY(), a.getZ()  * 16);
-            Optional<BlockRayHit<World>> c = BlockRay.from(b).stopFilter(BlockRay.onlyAirFilter()).to(a.getPosition().sub(a.getX(), 1, a.getZ())).end();
+            Optional<BlockRayHit<World>> c = BlockRay.from(b).stopFilter(BlockRay.onlyAirFilter()).to(a.getPosition().sub(b.getX(), 1, b.getZ()))
+                    .end();
 
             if (c.isPresent()) {
                 BlockRayHit<World> d = c.get();
