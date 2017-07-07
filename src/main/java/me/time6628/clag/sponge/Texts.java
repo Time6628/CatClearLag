3d744cc88c2146947c1a67bf9d3ba286c6197a2d
Time6628 <pturchan@yahoo.com>
2017-07-06 23:58:09 -0400

    fix warning template

--- src/main/java/me/time6628/clag/sponge/Texts.java
+++ src/main/java/me/time6628/clag/sponge/Texts.java
@@ -61,7 +61,8 @@ public class Texts {
     }
 
     public static TextTemplate warningMessage = TextTemplate.of(
-            prefix, warningColor, "Ground items will be cleared in ",
+            prefix,
+            warningColor, "Ground items will be cleared in ",
             arg("seconds").color(secondsColor),
             warningColor, " seconds."
     );
