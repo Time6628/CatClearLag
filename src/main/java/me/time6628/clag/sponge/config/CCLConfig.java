f01e45df2353145f473b0b4096c533b341efc571
Time6628 <pturchan1@gmail.com>
2017-09-16 21:58:22 -0400

    fix config

--- src/main/java/me/time6628/clag/sponge/config/CCLConfig.java
+++ src/main/java/me/time6628/clag/sponge/config/CCLConfig.java
@@ -8,47 +8,47 @@ import org.spongepowered.api.item.ItemTypes;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 @ConfigSerializable
 public class CCLConfig {
 
     @Setting("Interval")
-    public final int interval = 10;
+    public int interval = 10;
 
     @Setting(value = "Warnings")
-    public final List<Integer> warnings = new ArrayList<Integer>() {{
+    public List<Integer> warnings = new ArrayList<Integer>() {{
         add(540);
         add(570);
     }};
 
     @Setting("Whitelist")
-    public final List<String> whitelist = new ArrayList<String>() {{
+    public List<String> whitelist = new ArrayList<String>() {{
         add(ItemTypes.DIAMOND.getId());
         add(BlockTypes.DIAMOND_BLOCK.getId());
         add(BlockTypes.BEACON.getDefaultState().getId());
     }};
 
     @Setting("Entity Whitelist")
-    public final List<String> entityWhiteList = new ArrayList<String>(){{add(EntityTypes.BOAT.getId());}};
+    public List<String> entityWhiteList = new ArrayList<String>(){{add(EntityTypes.BOAT.getId());}};
 
     @Setting("Limits")
-    public final
-    Limits limits = new Limits();
+    public Limits limits = new Limits();
 
     @ConfigSerializable
     public static class Limits {
 
         @Setting("Max Mobs Per Chunk")
-        public final int mobLimitPerChunk = 20;
+        public int mobLimitPerChunk = 20;
 
         @Setting("Hostile Limit")
-        public final int hostileLimit = 500;
+        public int hostileLimit = 500;
 
         @Setting("Entity Check Interval")
-        public final int entityCheckInterval = 5;
+        public int entityCheckInterval = 5;
 
         @Setting("XP Orb Limit")
-        public final int maxXPOrbs = 300;
+        public int maxXPOrbs = 300;
     }
 
 }
